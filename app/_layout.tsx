6688929edeffbcef6a8ec5c266442fe47a629dae
Laxman Suthar <laxmansuthar365@gmail.com>
2025-09-07 22:33:00 +0530

    minor changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout() {
           <Stack.Screen name="(tabs)" />
           <Stack.Screen name="onboarding" />
           <Stack.Screen name="auth" />
+          
           <Stack.Screen name="+not-found" />
         </Stack>
       </GestureHandlerRootView>
